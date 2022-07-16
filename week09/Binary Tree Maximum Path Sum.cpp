ed03bf9636d2ecd44f6607e97991998d5f3682c5
Md Shohanur Rahman <93418530+Shohan07@users.noreply.github.com>
2022-07-16 23:17:40 +0600

    Add files via upload

---
+++ week09/Binary Tree Maximum Path Sum.cpp
@@ -0,0 +1,23 @@
+//TC : O(n) where n = number of nodes
+//SC : O(1)
+
+class Solution {
+public:
+    int dfs(TreeNode* root, int &mxSum){
+        if(!root)return 0;
+        
+        int lMax = dfs(root -> left, mxSum);
+        int rMax = dfs(root -> right, mxSum);
+        lMax = max(lMax, 0);
+        rMax = max(rMax, 0);
+        
+        mxSum = max(mxSum, lMax + rMax + root -> val);
+        return root -> val + max(lMax, rMax);
+    }
+    
+    int maxPathSum(TreeNode* root) {
+        int mxSum = INT_MIN;
+        dfs(root, mxSum);
+        return mxSum;
+    }
+};
